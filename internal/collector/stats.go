2e48717809163efec7a9314fdba7af7217e28ee3
Nebojsa <63752654+nradojcic@users.noreply.github.com>
2026-02-16 20:43:20 -0500

    Comments cleanup

--- internal/collector/stats.go
+++ internal/collector/stats.go
@@ -13,34 +13,34 @@ type Stats struct {
 	RAM float64
 }
 
-// StatsCollector defines an interface for collecting system stats.
+// StatsCollector defines an interface for collecting system stats
 // This allows for mocking in tests
 type StatsCollector interface {
 	CPUPercent() ([]float64, error)
 	VirtualMemory() (*mem.VirtualMemoryStat, error)
 }
 
-// GopsutilCollector is a real implementation of StatsCollector using gopsutil.
+// GopsutilCollector is a real implementation of StatsCollector using gopsutil
 type GopsutilCollector struct{}
 
-// CPUPercent implements the StatsCollector interface for CPU stats.
+// CPUPercent implements the StatsCollector interface for CPU stats
 func (c GopsutilCollector) CPUPercent() ([]float64, error) {
 	// For 0 duration, it reads overall CPU usage since last call or system boot
 	return cpu.Percent(time.Duration(0), false)
 }
 
-// VirtualMemory implements the StatsCollector interface for memory stats.
+// VirtualMemory implements the StatsCollector interface for memory stats
 func (c GopsutilCollector) VirtualMemory() (*mem.VirtualMemoryStat, error) {
 	return mem.VirtualMemory()
 }
 
-// GetStats collects current CPU and RAM usage using the real gopsutil collector.
+// GetStats collects current CPU and RAM usage using the real gopsutil collector
 // The public-facing function
 func GetStats() (Stats, error) {
 	return getStats(GopsutilCollector{})
 }
 
-// getStats is the internal, testable function that contains the core logic.
+// getStats is the internal, testable function that contains the core logic
 func getStats(collector StatsCollector) (Stats, error) {
 	cpuPercent, err := collector.CPUPercent()
 	if err != nil {
